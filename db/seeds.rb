fe4be28ef74b1538e3591faeeb19bfd68ba2348b
Eric Sipple <saalon@gmail.com>
2010-08-15 14:34:46 -0400

    Added seeds for linked records, and used it for Mecha Servos

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,47 @@ def seed_model(model, file, field_mapping_hash)
   end
 end
 
+def seed_children_models(parent_model, children_models, children_model_files, field_mapping_hashes, search_field)
+
+  children_models.each do |child_model|
+
+    field_mapping_hash = field_mapping_hashes[child_model]
+
+    csv(children_model_files[child_model]).each do |row|
+      parent_record = parent_model.find(:first, :conditions => "#{search_field} = '#{row[0]}'")
+      foreign_key = ("#{parent_model.name.underscore}_id").to_sym
+
+      record_creation_hash = row_hash(field_mapping_hash, row)
+      record_creation_hash[foreign_key] = parent_record.id
+      child_model.create!(record_creation_hash)
+    end
+
+  end
+  
+end
+
+def seed_linked_model(parent_model, parent_file, children_models, children_model_files, field_mapping_hashes, search_field)
+  delete_children_models(children_models)
+  parent_model.delete_all
+
+  seed_model(parent_model, parent_file, field_mapping_hashes[parent_model])
+
+  seed_children_models(parent_model, children_models, children_model_files, field_mapping_hashes, search_field)
+  
+end
+
 def row_hash(field_mapping_hash, row)
   hash = {}
   field_mapping_hash.each do |k,v|
      hash[k] = row[v]
   end
   hash
+end
+
+def delete_children_models(children_models)
+  children_models.each do |model|
+    model.delete_all
+  end
 end
 
 
@@ -51,4 +86,9 @@ seed_model(MechaMovementData, "mecha_movement_data.csv", {
 
 seed_model(MechaSensorData, "mecha_sensor_data.csv", {
         :sensor => 0, :cost => 1, :space => 2, :kills => 3, :weight => 4, :range => 5, :communication_range => 6})
+
+seed_linked_model(MechaServoData, "mecha_servo_data.csv", [MechaServoLevelData],
+                  {MechaServoLevelData => "mecha_servo_level_data.csv"}, {MechaServoData => {
+                :servo => 0}, MechaServoLevelData => {
+                :level => 1, :cost => 2, :space => 3, :kills => 4, :weight => 5}}, 'servo')
 
