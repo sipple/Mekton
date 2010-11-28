880ff97327c784243eff3f96933e0d78b6d9e3de
Eric Sipple <saalon@gmail.com>
2010-11-27 20:27:32 -0500

    Changed case statement in Mekton::SelectOptions to a send call

--- lib/mekton/select_options.rb
+++ lib/mekton/select_options.rb
@@ -6,20 +6,12 @@ module Mekton
       hash = ActiveSupport::OrderedHash.new("")
       hash[""] = ""
 
-      case model
-        when "character_armors" then options = get_armor_options(id, hash)
-        when "character_weapons" then options = get_weapon_options(id, hash)
-        when "character_equipments" then options = get_equipment_options(id, hash)
-        when "character_skills" then options = get_skill_options(id, hash)
-        
-      end
-
-      options
+      send("get_#{model}_options", id, hash)
     end
 
     private
 
-    def get_armor_options(id, hash)
+    def get_character_armors_options(id, hash)
       character_armor = CharacterArmor.find(id)
       CharacterArmorData.all(:order => 'armor').each {|armor_data| hash[armor_data.id.to_s] = armor_data.armor}
       if character_armor.character_armor_data_id
@@ -31,7 +23,7 @@ module Mekton
       hash
     end
 
-    def get_weapon_options(id, hash)
+    def get_character_weapons_options(id, hash)
       character_weapon = CharacterWeapon.find(id)
       CharacterWeaponData.all(:order => 'weapon').each {|weapon_data| hash[weapon_data.id.to_s] = weapon_data.weapon}
       if character_weapon.character_weapon_data_id
@@ -43,7 +35,7 @@ module Mekton
       hash
     end
 
-    def get_equipment_options(id, hash)
+    def get_character_equipments_options(id, hash)
       character_equipment = CharacterEquipment.find(id)
       CharacterEquipmentData.all(:order => 'equipment').each {|equipment_data| hash[equipment_data.id.to_s] = equipment_data.equipment}
       if character_equipment.character_equipment_data_id
@@ -55,7 +47,7 @@ module Mekton
       hash
     end
 
-    def get_skill_options(id, hash)
+    def get_character_skills_options(id, hash)
       character_skill = CharacterSkill.find(id)
       CharacterSkillData.all(:order => 'skill').each {|skill_data| hash[skill_data.id.to_s] = skill_data.skill}
       if character_skill.character_skill_data_id
