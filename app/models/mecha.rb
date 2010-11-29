e41299a221272dd06676e292e822b67bc01d9683
Eric Sipple <saalon@gmail.com>
2010-11-28 20:30:12 -0500

    Added a mecha stats table.  It'll need more work.

--- app/models/mecha.rb
+++ app/models/mecha.rb
@@ -11,13 +11,58 @@ class Mecha < ActiveRecord::Base
 
   belongs_to :character
 
+  BASE_LAND_MA = 6
+  BASE_FLIGHT_MA = 0
+  BASE_MV = -1
+
 
   def weight
     # self.mecha_objects.all.sum(&:field)
+    0
   end
 
   def mv
-    -1
+    case weight
+      when 0..19
+        -1
+      when 20..29
+        -2
+      when 30..39
+        -3
+      when 40..49
+        -4
+      when 50..59
+        -5
+      when 60..69
+        -6
+      when 70..79
+        -7
+      when 80..89
+        -8
+      when 90..99
+        -9
+      else
+        -10
+    end
+  end
+
+  def land_ma
+    case weight
+      when 0..19
+        6
+      when 20..39
+        5
+      when 40..59
+        4
+      when 60..79
+        3
+      else
+        2
+    end
+  end
+
+  def flight_ma
+    0
   end
 
   def mecha_reflexes
@@ -43,6 +88,5 @@ class Mecha < ActiveRecord::Base
   def mecha_missiles
     self.character.mecha_missiles + self.mecha_reflexes
   end
-
   
 end
