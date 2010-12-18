b1f7d2ff03e260c7f94d12b743f588ab1464737d
Eric Sipple <saalon@gmail.com>
2010-12-18 16:51:23 -0500

    Fixed movement system active scope

--- app/models/mecha_movement_data.rb
+++ app/models/mecha_movement_data.rb
@@ -1,3 +1,3 @@
 class MechaMovementData < ActiveRecord::Base
-  named_scope :active, :conditions => {:disabled => false}, :order => 'movement ASC'
+  named_scope :active, :conditions => {:disabled => false}, :order => 'movement_system ASC'
 end
